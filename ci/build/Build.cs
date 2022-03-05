2fa92c950905f0b1c9c7bf733c5cc43e6e92ffea
Momo the Monster <momodmonster@gmail.com>
2022-03-04 18:24:34 -0800

    Adds CI action to build Resources page from Notion DB

--- ci/build/Build.cs
+++ ci/build/Build.cs
@@ -1,10 +1,14 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Notion.Client;
 using Nuke.Common;
 using Nuke.Common.IO;
 using Nuke.Common.Utilities;
@@ -24,6 +28,24 @@ class Build : NukeBuild
     private AbsolutePath _markdownSourceDir = RootDirectory.Parent / "docs";
     private AbsolutePath _readmeDocsDir = RootDirectory / "_temp_Readme";
     private AbsolutePath _githubTempDir = RootDirectory / "_temp_GHWiki";
+
+    [Parameter("Database from which to read OSC Resources")]
+    private string _notionDB;
+    [Parameter("Secret for Integration")]
+    private string _notionSecret;
+    
+    static JObject _localSecrets;
+    static JObject LocalSecrets {
+        get
+        {
+            if (_localSecrets == null)
+            {
+                string json = File.ReadAllText(BuildProjectDirectory / "secrets.json");
+                _localSecrets = JObject.Parse(json);
+            }
+            return _localSecrets;
+        }
+    }
 
     /// <summary>
     /// This works for now - we could instead to a prepass of all files to build this from each page's Front Matter
@@ -99,6 +121,7 @@ class Build : NukeBuild
     /// Use frontmatter to change page slugs
     /// </summary>
     Target GitHubWikiToReadmeDotCom => _ => _
+        .DependsOn(UpdateResourcesFromNotion)
         .Executes(() =>
         {
             FileSystemTasks.EnsureExistingDirectory(_readmeDocsDir);
@@ -127,6 +150,7 @@ class Build : NukeBuild
         });
 
     Target PushReadmeUpdates => _ => _
+        .DependsOn(UpdateResourcesFromNotion)
         .DependsOn(GitHubWikiToReadmeDotCom)
         .Executes(async () =>
         {
@@ -137,5 +161,113 @@ class Build : NukeBuild
             {
                 await SendPageToReadme(info.FullName);
             }
+        });
+    
+    
+    
+    Target LoadSecrets => _ => _
+        .Executes(() =>
+        {
+            if (IsLocalBuild)
+            {
+                _notionSecret = LocalSecrets["NOTION_SECRET"]?.ToString();
+                _notionDB = LocalSecrets["NOTION_DB"]?.ToString();
+            }
+        });
+
+    Target UpdateResourcesFromNotion => _ => _
+        .DependsOn(LoadSecrets)
+        .Executes(async () =>
+        {
+            var client = NotionClientFactory.Create(new ClientOptions
+            {
+                AuthToken = _notionSecret
+            });
+            
+            var selectFilter = new SelectFilter("Status", equal: "Approved");
+            var filter = new CompoundFilter(and: new List<Filter>(){selectFilter});
+            var queryParams = new DatabasesQueryParameters { Filter = filter};
+            var pages = await client.Databases.QueryAsync(_notionDB, queryParams);
+            
+            // Turn database into SortedDictionary
+            var resources = new SortedDictionary<string, List<string>>();
+            foreach (Page page in pages.Results)
+            {
+                var nameValue = page.Properties["Name"] as TitlePropertyValue;
+                if (nameValue == null || nameValue.Title.Count == 0)
+                {
+                    Console.WriteLine($"Couldn't read name of {page.Id}");
+                    continue;
+                }
+                var name = nameValue.Title[0].PlainText;
+                
+                var descriptionValue = page.Properties["Description"] as RichTextPropertyValue;
+                if (descriptionValue == null || descriptionValue.RichText.Count == 0)
+                {
+                    Console.WriteLine($"Couldn't read description of {name}");
+                    continue;
+                }
+                var description = descriptionValue.RichText[0].PlainText;
+                
+                var linkValue = page.Properties["Link"] as UrlPropertyValue;
+                if (linkValue == null)
+                {
+                    Console.WriteLine($"Couldn't read Link of {name}");
+                    continue;
+                }
+
+                var link = linkValue.Url;
+
+                var languagesValue = page.Properties["Language"] as MultiSelectPropertyValue;
+                if (languagesValue == null)
+                {
+                    Console.WriteLine($"Couldn't read Languages of {name}");
+                    continue;
+                }
+                var languages =
+                    languagesValue.MultiSelect.Select(entry => entry.Name);
+                var languageString = string.Join(",",languages);
+
+                var publicCategoryValue = page.Properties["Public Category"] as SelectPropertyValue;
+                if (publicCategoryValue == null)
+                {
+                    Console.WriteLine($"Couldn't read Public Category of {name}");
+                    continue;
+                }
+                var publicCategory = publicCategoryValue.Select.Name;
+
+                string entry = $"* [{name}]({link}): {description} ({languageString})";
+
+                if (resources.ContainsKey(publicCategory))
+                {
+                    resources[publicCategory].Add(entry);
+                }
+                else
+                {
+                    resources.Add(publicCategory, new List<string>(){entry});
+                }
+            }
+            
+            // Combine categories and entries into string
+            StringBuilder builder = new StringBuilder();
+            foreach (var category in resources)
+            {
+                // Add category header
+                builder.AppendLine($"# {category.Key}");
+                
+                // Add all items
+                foreach (string entry in category.Value)
+                {
+                    builder.AppendLine(entry);
+                }
+
+                builder.AppendLine(); // Adding a space at end of each category
+            }
+
+            // Write to target page
+            var targetFile = _markdownSourceDir / "Resources.md";
+            var contents = File.ReadAllText(targetFile);
+            contents = contents.Replace("{{Resources}}", builder.ToString());
+            File.WriteAllText(targetFile, contents);
         });
 }
